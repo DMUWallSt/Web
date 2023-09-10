44741ad056dbcfc7dbe895d1ed4fefba0fbae6d2
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-09-10 23:56:30 +0900

    Fix : 워드클라우드 툴팁 제거,폰트 변경 및 위치 조정

--- src/reactwordcloud.js
+++ src/reactwordcloud.js
@@ -2,30 +2,37 @@ import ReactWordcloud from "react-wordcloud";
 import { useNavigate } from "react-router-dom";
 
 function MyWordcloud(props) {
+  const size = [600, 400];
   const navi = useNavigate();
   const words = props.words;
   console.log(words);
   const callbacks = {
     getWordColor: (word) => (word.value > 10 ? "red" : "blue"),
     onWordClick: (word) => navi(word.url),
-    onWordMouseOver: console.log,
     getWordTooltip: (word) => `${word.value}`,
   };
   const options = {
     rotations: 1,
     rotationAngles: [0, 0],
     fontSizes: [5, 100],
+    enableTooltip: false,
+    fontFamily: "GangwonEduPowerExtraBoldA",
   };
-  const size = [600, 400];
 
   return (
-    <div style={{ marginLeft: "500px" }}>
+    <div>
       <ReactWordcloud
         callbacks={callbacks}
         options={options}
         size={size}
         minSize={[1000, 500]}
         words={words}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          height: "100vh",
+        }}
       />
     </div>
   );
