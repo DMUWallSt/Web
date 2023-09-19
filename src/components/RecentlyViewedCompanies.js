1e206871318995652bf27de66ea5e96b438a2795
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-09-19 20:50:46 +0900

    Design : 최근 목록 디자인 개선

--- src/components/RecentlyViewedCompanies.js
+++ src/components/RecentlyViewedCompanies.js
@@ -3,14 +3,21 @@ import styled from "styled-components";
 import { useEffect } from "react";
 const RecentlyVisitedCompanies = () => {
   const RecentlyViewedCompaniesDiv = styled.div`
+    background-color: rgb(13, 110, 253);
+    width: 150px;
     margin-left: 30px;
+    color: white;
+    text-align: center;
+    font-family: "TTTogether";
+    font-weight: 100;
+    border-radius: 20px;
   `;
-  
+
   return (
-    <RecentlyViewedCompaniesDiv style={{ backgroundColor: "lightblue" }}>
-      <h4>최근 방문한 기업 목록</h4>
-      {JSON.parse(sessionStorage.getItem('recentlyViewed')).map((n)=>{
-        return <div>{n}</div>
+    <RecentlyViewedCompaniesDiv>
+      <h5 style={{ marginTop: "10px" }}>최근 방문 목록</h5>
+      {JSON.parse(sessionStorage.getItem("recentlyViewed")).map((n) => {
+        return <div>{n}</div>;
       })}
     </RecentlyViewedCompaniesDiv>
   );
