30c1a6b658b7c408de372b29934b86a6bdb0037d
ChoiSeungWook <agion427@gmail.com>
2023-09-20 16:29:29 +0900

    Feat: Modal기능 구현

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,98 +1,19 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import Modal from 'react-modal';
 
-export const ModalContainer = styled.div`
-  display : flex;
-  justify-content : center;
-  align-items : center;
-  height : 100%;
-`;
+Modal.setAppElement('#root'); // 모달이 화면에 렌더링될 DOM 요소를 설정
 
-export const ModalBackdrop = styled.div`
-  z-index: 1; 
-  position: fixed;
-  display : flex;
-  justify-content : center;
-  align-items : center;
-  background-color: rgba(0,0,0,0.4);
-  border-radius: 10px;
-  top : 0;
-  left : 0;
-  right : 0;
-  bottom : 0;
-`;
-
-export const ModalBtn = styled.button`
-  background-color: var(--coz-purple-600);
-  text-decoration: none;
-  border: none;
-  padding: 20px;
-  color: black;
-  border-radius: 30px;
-  cursor: grab;
-`;
-
-export const ExitBtn = styled(ModalBtn) `
-background-color : #4000c7;
-border-radius: 10px;
-text-decoration: none;
-margin: 10px;
-padding: 5px 10px;
-width: 40px;
-height: 40px;
-display : flex;
-justify-content : center;
-align-items : center;
-`;
-
-export const ModalView = styled.div.attrs((props) => ({
-  // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있다.
-  role: 'dialog',
-}))`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  border-radius: 20px;
-  width: 500px;
-  heigth: 200px;
-  background-color: #ffffff;
-    >div.desc {
-      margin: 50px;
-      font-size: 20px;
-      color: var(--coz-purple-600);
-    }
-`;
-
-export const Modal = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openModalHandler = () => {
-    // isOpen의 상태를 변경하는 메소드를 구현
-    // !false -> !true -> !false
-    setIsOpen(!isOpen) 
-  };
-
+function ModalComponent(props) {
   return (
-    <>
-      <ModalContainer>
-        <ModalBtn onClick={openModalHandler}
-        // 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다. 
-        > Modal Button
-          {/* 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때는 ModalBtn의 내부 텍스트가 'Opened!' 로 Modal이 닫힌 상태(isOpen이 false인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Open Modal'이 되도록 구현 */}
-        </ModalBtn>
-        {/* 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현 */}
-        {isOpen ? 
-        <ModalBackdrop onClick={openModalHandler}>
-            <ModalView onClick={(e) => e.stopPropagation()}>
-              <ExitBtn onClick={openModalHandler}>x</ExitBtn>
-              <div className='desc'>Modal Test</div>
-            </ModalView>
-          </ModalBackdrop>
-          : null
-        }
-      </ModalContainer>
-    </>
+    <Modal
+      isOpen={props.isOpen} // 모달이 열려 있는지 여부를 결정하는 prop
+      onRequestClose={props.onRequestClose} // 모달 닫기 요청 시 호출되는 콜백
+    >
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+      <button onClick={props.onRequestClose}>닫기</button>
+    </Modal>
   );
-};
+}
 
-export default Modal;
+export default ModalComponent;
