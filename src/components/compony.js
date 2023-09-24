1cf9c1f09d87a442692f4e5546c1f8184e099bca
ChoiSeungWook <agion427@gmail.com>
2023-09-24 23:35:13 +0900

    Design: compony info

--- src/components/compony.js
+++ src/components/compony.js
@@ -1,59 +1,94 @@
 import React from "react";
 import styled from "styled-components";
-import test_logo from "./test_logo.jpg";
+import test_logo from "./test_logo.png";
+import chart from "./chart_dummy.png"
 
 const ComponyBlock = styled.div`
-  margin: 20px;
-  //border-bottom: 1px solid black;
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+
+  .title{
+    display: flex;
+    flex-direction: row;
+    margin: 0 auto;
+  }
+  .stockinfo {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+  .score {
+    display: flex;
+    flex-direction: column;
+    //border-right: 1px solid black;
+  }
+  .stock {
+    display: flex;
+    flex-direction: column;
+
+  }
 
   img {
-      width: 150px;
-    height: 100px;
+    width: 300px;
+    height: 30px;
+    object-fit: contain;
+    margin: 0 auto;
+    margin-bottom: 10px;
   }
-  h1 {
-    margin: 0;
+  h1 #stockname {
     font-size: 36px;
     color: #343a40;
+    text-align: center;
+  }
+  .title #stockid {
+    margin-left: 10px;
+
   }
   .content {
     margin-top: 4px;
     color: #868e96;
-    font-size: 21px;
-  }
-  .code {
-    color: #868e96;
     font-size: 18px;
-    margin-top: 4px;
   }
-  .stock {
-    color: #d90429;
-    font-size: 30px;
-    margin-top: 4px;
-    font-weight: bold;
+  #chart {
+    width: 800px;
+    height: 300px;
+    object-fit: contain;
+    margin: 0 auto;
+    margin-bottom: 10px;
   }
-  .var {
-    color: #d90429;
-    font-size: 14px;
-    margin-top: 4px;
-  }
+  
 `;
 
 function Componyinfo() {
   return (
     <ComponyBlock>
       <img src={test_logo} alt="test logo" />
-      <h1>삼성생명032830</h1>
-      <div className="content">
-        1957년에 설립되어 업계 최대의 전속 설계사 조직과 계리 전문인력을
-        보유하고 있는 삼성그룹 계열의 생명보험사. 보장성 및 연금/저축성 보험상품
-        서비스 제공을 주요 사업으로 하며, 압도적인 브랜드 파워와 안정적인 영업
-        기반을 바탕으로 업계 최상위 시장 지위를 유지하고 있음. 연결대상 주요
-        종속회사로 삼성카드, 삼성자산운용, 삼성생명 태국법인,
-        북경삼성치업유한공사, 삼성SRA자산운용 등이 있음.
+      <div className="title">
+        <h1 id="stockname">삼성전자</h1>
+        <p id="stockid">005930 | KOSPI</p>
       </div>
-      <div className="newscom">KOSPI</div>
-      <div className="stock">68,300</div>
-      <div className="var">전일대비 +800</div>
+      <div className="content">
+        <p>
+        한국 및 DX부문 해외 9개 지역총괄과 DS부문 해외 5개 지역총괄, SDC, Harman 등 230개의 종속기업으로 구성된 글로벌 전자기업임.
+        세트사업은 TV를 비롯 모니터, 냉장고, 세탁기, 에어컨, 스마트폰, 네트워크시스템, 컴퓨터 등을 생산하는 DX부문이 있음.
+        부품 사업에는 DRAM, NAND Flash, 모바일AP 등의 제품을 생산하고 있는 DS 부문과 중소형OLED 등의 디스플레이 패널을 생산하고 있는 SDC가 있음.
+        </p>
+      </div>
+      <div className="stockinfo">
+        <div className="score">
+          <h1>투자점수</h1>
+          <h3>재무점수 | 100</h3>
+          <h3>최근점수 | 100</h3>
+          <h3>총 점수 | 100</h3>
+          <p> 투자에 문제가 없는 점수입니다.</p>
+        </div>
+        <div className="stock">
+          <h1>71,400</h1>
+          <p>전일대비 ▲500 | +0.71%</p>
+        </div>
+      </div>
+      <img id="chart" src={chart} alt="chart"/>
     </ComponyBlock>
   );
 }
