6943f286f1a742788f69fa429c421bc2ff2a168a
ChoiSeungWook <agion427@gmail.com>
2023-09-25 18:43:49 +0900

    Design: 디자인 수정

--- src/components/company.js
+++ src/components/company.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import test_logo from "./test_logo.png";
+import test_logo from "./test_lg.png";
 import chart from "./chart_dummy.png"
 
 const CompanyBlock = styled.div`
@@ -12,13 +12,14 @@ const CompanyBlock = styled.div`
   .title{
     display: flex;
     flex-direction: row;
-    margin: 0 auto;
   }
+
   .stockinfo {
     display: flex;
     flex-direction: row;
-    //justify-content: space-around;
+    justify-content: space-around;
   }
+
   .score {
     display: flex;
     flex-direction: column;
@@ -29,14 +30,13 @@ const CompanyBlock = styled.div`
   .stock {
     display: flex;
     flex-direction: column;
-
+    margin: 0 auto;
   }
 
   img {
-    width: 300px;
-    height: 30px;
+    width: 100px;
+    height: 50px;
     object-fit: contain;
-    margin: 0 auto;
     margin-bottom: 10px;
   }
   h1 #stockname {
@@ -47,14 +47,13 @@ const CompanyBlock = styled.div`
   }
   .title #stockid {
     margin-left: 10px;
-    margin-top: 10%;
-
+    padding-top: 23px;
   }
   .content {
-    margin-top: 4px;
     color: #868e96;
     font-size: 18px;
   }
+
   #chart {
     width: 800px;
     height: 300px;
@@ -62,10 +61,14 @@ const CompanyBlock = styled.div`
     margin: 0 auto;
     margin-bottom: 10px;
   }
+
   .stockinfo .stock {
     text-align: center;
     justify-content: center;
-    padding-left: 150px;
+  }
+
+  #stocktoday {
+    color: red;
   }
   
 `;
@@ -75,14 +78,14 @@ function Companyinfo() {
     <CompanyBlock>
       <img src={test_logo} alt="test logo" />
       <div className="title">
-        <h1 id="stockname">삼성전자</h1>
-        <p id="stockid">005930 | KOSPI</p>
+        <h1 id="stockname">LG전자</h1>
+        <p id="stockid">066570 | KOSPI</p>
       </div>
       <div className="content">
         <p>
-        한국 및 DX부문 해외 9개 지역총괄과 DS부문 해외 5개 지역총괄, SDC, Harman 등 230개의 종속기업으로 구성된 글로벌 전자기업임.
-        세트사업은 TV를 비롯 모니터, 냉장고, 세탁기, 에어컨, 스마트폰, 네트워크시스템, 컴퓨터 등을 생산하는 DX부문이 있음.
-        부품 사업에는 DRAM, NAND Flash, 모바일AP 등의 제품을 생산하고 있는 DS 부문과 중소형OLED 등의 디스플레이 패널을 생산하고 있는 SDC가 있음.
+        동사와 종속기업의 주요사업부문은 Home Appliance & Air 솔루션, Home Entertainment, Mobile Communications, Vehicle component 솔루션 등 6개로 구분.
+        OLED TV는 초슬림, 월페이퍼, 롤러블 TV 등 지속적인 혁신 제품 출시로 프리미엄 시장을 지속 선도하고 있음.
+        디스플레이 오디오와 내비게이션 영역에서는 동사의 디스플레이 및 소프트웨어 역량을 활용하여 제품 차별화중.
         </p>
       </div>
       <div className="stockinfo">
@@ -94,8 +97,8 @@ function Companyinfo() {
           <p> 투자에 문제가 없는 점수입니다.</p>
         </div>
         <div className="stock">
-          <h1>71,400</h1>
-          <p>전일대비 ▲500 | +0.71%</p>
+          <h1 id="stocktoday">103,400</h1>
+          <p>전일대비 ▲300 | + 0.29%</p>
         </div>
       </div>
       <img id="chart" src={chart} alt="chart"/>
