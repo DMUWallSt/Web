fc4d69ee3019f94e8d0aeae0a8720853f23a151d
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-10-13 23:20:41 +0900

    Feat : 이전 페이지 기능

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,11 +1,10 @@
 import "./news.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import BoxTemplate from "../components/box";
 import NewsHead from "../components/newsHead";
 import Companyinfo from "../components/company";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import styled from "styled-components";
@@ -13,31 +12,65 @@ import styled from "styled-components";
 function News(props) {
   const { id } = useParams();
 
+  const navi = useNavigate();
+
   const PaginationBox = styled.div`
-    //페이지네이션을 포함한 박스
+    // 페이지네이션을 포함한 박스
     display: flex;
     flex-direction: column;
+    align-items: center; /* Center horizontally */
+    height: 100vh; /* Optional to make the container full height */
+  `;
+
+  const NotFoundBox = styled.div`
+    text-align: center;
+    background-color: #007bff;
+  `;
+  const NotFoundBoxH1 = styled.h1`
+    margin: 10px;
+    color: white;
+    font-family: "NOTO";
+    padding: 5px;
+  `;
+
+  const NotFoundBoxH3 = styled.h3`
+    margin: 10px;
+    color: white;
+    font-family: "NOTO";
+  `;
+
+  const GoBackButton = styled.button`
+    text-align: center;
+    background-color: #007bff;
+    border-radius: 5px;
+    width: 13vw;
+    border: none;
   `;
 
   const [newsData, setNewsData] = useState([]);
+  const [dataSuccess, setDataSuccess] = useState(false);
+  const [loading, setLoading] = useState(false); // axios에서 정보를 받아오고 랜더링하기 위한 상태 state
+  const [error, setError] = useState(null); // 에러발생시 에러를 저장할 수 있는 state
+
   useEffect(() => {
     // 데이터를 비동기로 가져옵니다.
+    setLoading(true);
     async function fetchNewsData() {
-      //이걸로 페이지네이션도 할 수 있음
       try {
-        // const response = await axios.get(
-        //   "http://localhost:3000/dummy/samsung.json"
-        // );
-        const response = await axios.get(`http://localhost:3001/news/${id}`);
-        //나중에 api 개발 된 뒤, App.js 의 companyData 에서 기업 이름을 가져와서 변수화 한 후 넣으면 될듯.
+        const response = await axios.get(`http://localhost:3002/news/${id}`);
+        setDataSuccess(true);
         console.log(response.data);
+        setLoading(false);
         setNewsData(response.data);
       } catch (error) {
+        setLoading(false);
+        setError(error);
+        setDataSuccess(false);
         console.error(error);
       }
     }
     fetchNewsData(); // 데이터 가져오는 함수 호출
-  }, []); // 빈 배열을 넣어 한 번만 호출되도록 설정
+  }, []);
 
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
 
@@ -48,62 +81,69 @@ function News(props) {
 
   const newsPerPage = 5;
   const currentPageLast = currentPage * newsPerPage; // 현재 페이지의 처음
-  const currentPageFirst = currentPageLast - newsPerPage; /// 현재 페이지의 끝
-  const currentNews = newsData.slice(currentPageFirst, currentPageLast); //배열의 begin 부터 end 까지(end 미포함)에 대한 얕은 복사본을 새로운 배열 객체로 반환
-  const pageNumber = Math.ceil(newsData.length / newsPerPage); // 총 페이지 수(소수점 올림)
+  const currentPageFirst = currentPageLast - newsPerPage; // 현재 페이지의 끝
+  const currentNews = newsData.slice(currentPageFirst, currentPageLast); // 배열의 begin부터 end까지의 얕은 복사본
+  const pageNumber = Math.ceil(newsData.length / newsPerPage); // 총 페이지 수(올림)
 
-  //const recentlyViewed =
-  //JSON.parse(sessionStorage.getItem("recentlyViewed")) || [];
-  //메인 페이지에 접솔했을 때 처음 접속이면 빈 배열, 아니면 기존 배열 사용
-
-  // if (!recentlyViewed.includes(props.companyData[id].text)) {
-  //   //중복 체크 로직. 추후 데이터 받아 와서 저장
-  //   recentlyViewed.unshift(props.companyData[id].text);
-  //   sessionStorage.setItem("recentlyViewed", JSON.stringify(recentlyViewed));
-  // }
-
-  //추후 여기에 useQuery 넣어서 각 기업 정보 페이지 별로 데이터 "하나만" 받아와서 출력되게 함.
-  //props로 데이터 넘겨줘서 표시하게 할 것.
-  //맨 처음에 XX 관련 페이지입니다. 표시해도 좋을듯.
-
+  if (loading)
+    return (
+      <NotFoundBox>
+        <NotFoundBoxH1>기업 정보를 받아오는 중입니다.....</NotFoundBoxH1>
+      </NotFoundBox>
+    );
+  if (error)
+    return (
+      <div style={{ textAlign: "center" }}>
+        <NotFoundBox>
+          <NotFoundBoxH1>
+            기업이 존재하지 않습니다. 정확한 기업명을 입력해 주세요.
+          </NotFoundBoxH1>
+        </NotFoundBox>
+        <GoBackButton
+          onClick={() => {
+            navi(-1);
+          }}
+        >
+          <NotFoundBoxH3>이전 페이지</NotFoundBoxH3>
+        </GoBackButton>
+      </div>
+    );
   return (
     <div className="main">
       <PaginationBox>
-        {/*props.companyData[id].url*/}
-        <BoxTemplate>
-          {currentNews &&
-            currentNews.map((n) => {
-              return <NewsHead key={n.toString()} newsData={n} id={n.id} />;
-            })}
-        </BoxTemplate>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <Stack spacing={2}>
-            <Pagination
-              count={pageNumber}
-              page={currentPage}
-              color="primary"
-              showFirstButton
-              showLastButton
-              onChange={handleChange}
-            />
-            {/*page: 현재 페이지
-            count: 총 페이지 개수
-            onChange: 페이지네이션에서 왼쪽, 오른쪽 화살표나 다른 페이지를 눌렀을 때 발생하는 함수
-            sx: 스타일을 정의
-            size: 전체적인 크기를 설정
-            renderItem: 페이지네이션 안을 직접 설정하고자 할 때 사용
-            https://flykimjiwon.tistory.com/112 */}
-          </Stack>
-        </div>
+        {dataSuccess ? (
+          <div>
+            <BoxTemplate>
+              {currentNews &&
+                currentNews.map((n) => {
+                  return <NewsHead key={n.toString()} newsData={n} id={n.id} />;
+                })}
+            </BoxTemplate>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Stack spacing={2}>
+                <Pagination
+                  count={pageNumber}
+                  page={currentPage}
+                  color="primary"
+                  showFirstButton
+                  showLastButton
+                  onChange={handleChange}
+                />
+              </Stack>
+            </div>
+          </div>
+        ) : null}
       </PaginationBox>
-      <BoxTemplate>
-        <Companyinfo />
-      </BoxTemplate>
+      {dataSuccess && (
+        <BoxTemplate>
+          <Companyinfo />
+        </BoxTemplate>
+      )}
     </div>
   );
 }
